217105c5b6341e6d852ddcfacfaea16e5a525f77
Neeraj Kumar Singh B <43318996+neerazz@users.noreply.github.com>
2021-04-23 17:11:56 -0400

    Commited React.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 import 'rsuite/dist/styles/rsuite-default.css';
 import MySidebar from "./components/Sidebar";
@@ -9,17 +9,15 @@ import { Container, Header, Content, Footer, Sidebar, Divider} from "rsuite";
 // import Blog from "./components/Blog";
 import Timeline from "./components/Timeline";
 
-class App extends Component {
+class App extends React.Component {
   render() {
+    // var sideBarStyle = { "height": "100vh" };
     return (
       <div>
         <Container>
           <Sidebar>
             <MySidebar></MySidebar>
           </Sidebar>
-          <Divider vertical />
-          <Container>
-            <Header>Header</Header>
             <Content>
               {/* <About></About> */}
               {/* <Projects></Projects> */}
@@ -27,8 +25,6 @@ class App extends Component {
               <Introduction></Introduction>
               <Timeline></Timeline>
             </Content>
-            <Footer>Footer</Footer>
-          </Container>
         </Container>
       </div>
     );
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,39 +1,36 @@
-import { Sidenav } from "rsuite";
+import { Sidebar, Sidenav } from "rsuite";
 import { Nav, Dropdown } from "rsuite";
 import { Icon } from "rsuite";
-import React from 'react';
+import React from "react";
 
 class MySidebar extends React.Component {
   render() {
-    var mystyle = {
-        width: 250,
-        height: 100%
-      };
+    var sideBarStyle = { "height": "100vh" };
     return (
-      <div style={{ height: 100% ; width: 250 }}>
-        <Sidenav defaultOpenKeys={["2", "3"]} activeKey="1">
-          <Sidenav.Body>
-            <Nav>
-              <Nav.Item eventKey="1" icon={<Icon icon="dashboard" />}>
-                Neeraj Kumar Singh
-              </Nav.Item>
-              <Dropdown eventKey="2" title="Introduction">
-                <Dropdown.Item eventKey="2-1">About</Dropdown.Item>
-                <Dropdown.Item eventKey="2-2">Timeline</Dropdown.Item>
-              </Dropdown>
-              <Dropdown eventKey="3" title="Contact">
-                <Dropdown.Item eventKey="3-1">Email</Dropdown.Item>
-                <Dropdown.Item eventKey="3-2">Linked-In</Dropdown.Item>
-                <Dropdown.Item eventKey="3-3">GitHub</Dropdown.Item>
-                <Dropdown.Item eventKey="3-4">Blogger</Dropdown.Item>
-                <Dropdown.Item eventKey="3-5">Contact</Dropdown.Item>
-              </Dropdown>
-            </Nav>
-          </Sidenav.Body>
-        </Sidenav>
+      <div>
+          <Sidenav defaultOpenKeys={["2", "3"]} activeKey="1">
+            <Sidenav.Body>
+              <Nav style={sideBarStyle}>
+                <Nav.Item eventKey="1" icon={<Icon icon="dashboard" />}>
+                  Neeraj Kumar Singh
+                </Nav.Item>
+                <Dropdown eventKey="2" title="Introduction">
+                  <Dropdown.Item eventKey="2-1">About</Dropdown.Item>
+                  <Dropdown.Item eventKey="2-2">Timeline</Dropdown.Item>
+                </Dropdown>
+                <Dropdown eventKey="3" title="Contact">
+                  <Dropdown.Item eventKey="3-1">Email</Dropdown.Item>
+                  <Dropdown.Item eventKey="3-2">Linked-In</Dropdown.Item>
+                  <Dropdown.Item eventKey="3-3">GitHub</Dropdown.Item>
+                  <Dropdown.Item eventKey="3-4">Blogger</Dropdown.Item>
+                  <Dropdown.Item eventKey="3-5">Contact</Dropdown.Item>
+                </Dropdown>
+              </Nav>
+            </Sidenav.Body>
+          </Sidenav>
       </div>
     );
   }
 }
 
-export default MySidebar
+export default MySidebar;
